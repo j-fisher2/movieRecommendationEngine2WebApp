8b29a41ed9acadc749486e1ae8d5c71f738de031
Justin <justinfisher340@gmail.com>
2023-11-01 00:59:06 -0400

    began notification alerts for user liking movie

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let btn=document.getElementById("likeb")
+    btn.addEventListener("click",()=>{
+    let message="You Liked a Movie!"
+    alert(message)
+})
+});
