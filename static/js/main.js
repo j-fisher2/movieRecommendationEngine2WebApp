3305c8d9378c054515c57472bf6210ac16b28ee3
Justin <justinfisher340@gmail.com>
2023-11-02 00:57:19 -0400

    enabled ajax requests for async movie likes

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let btn=document.getElementById("likeb")
-    btn.addEventListener("click",()=>{
-    let message="You Liked a Movie!"
-    alert(message)
-})
+    var likeForms = document.getElementsByClassName('movie-form');
+
+    for (let i = 0; i < likeForms.length; i++) {
+        var id = "like-button" + (i + 1); 
+
+        var movie_liked=false;
+
+        var likeButton = likeForms[i].querySelector('#' + id);
+        if(!likeButton){
+            likeButton=likeForms[i].querySelector('#like-button')
+        }
+
+        if (likeButton) {
+            likeButton.addEventListener("click", function(e) {
+                e.preventDefault();
+                var formData = new FormData(likeForms[i]);
+
+                fetch('/like-movie', {
+                    method: 'POST',
+                    body: formData,
+                }).then(res => res.json()).then(data => {
+                    console.log(data);
+                    alert("You liked a movie")
+                }).catch(err => {
+                    console.log(err);
+                })
+                fetch('/update-user-profile',{
+                    method:"POST",
+                    body:formData,
+                }).then(res=>res.json()).then(data=>console.log(data));
+            });
+        }
+    }
 });
