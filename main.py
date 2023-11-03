9f349652a84b9429f421b653d189585a3bbb1162
Justin <justinfisher340@gmail.com>
2023-11-03 08:56:16 -0400

    added compatibility for async movie likes, added topRecommendationsCache for users. modified algorithm to update itself (recommendations) when user likes movie

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def getIndexFromTitle(title):
     cursor.execute(query,values)
     result=cursor.fetchone()
     if result:
-        titleCache.insert(title,result[0])
+        titleCache.insert(title.lower(),result[0])
         return result[0]
     else:
         return "title not found"
@@ -137,7 +137,7 @@ def get_top_recommendations(movie,idx,collab_recs=False):
                 heapq.heappop(minHeap)
                 heapq.heappush(minHeap,[scores[i],getTitleFromIndex(i)])
     minHeap.sort(reverse=True)
-    minHeap=[[get_movie_poster(i[1].lower()),i[1]] for i in minHeap]
+    minHeap=[[get_movie_poster(i[1].lower()),i[1],i[0]] for i in minHeap]
     return minHeap
 
 def get_proper_title(lower_title):
@@ -154,6 +154,35 @@ def generate_list():
     titles=['Interstellar','Guardians of the Galaxy','Monsters University','Toy Story','The Avengers','Pearl Harbor','The Perfect Storm','300: Rise of an Empire','The Tourist','The Wolf of Wall Street','Divergent','The Hangover','Bedtime Stories','Grown Ups','The Lion King','Braveheart','The Vow','American Sniper','The Dictator','Marley & Me','Lilo & Stitch']
     titles=[title.lower() for title in titles]
     return titles
+
+def filter_movies(top_movies):  #user first likes movies
+    #save final list and scores within recommendations table
+    user=session['user']
+    cursor=mysql.get_db().cursor()
+    added=set()
+    topRecommendationCache[user]=set()
+    for m in top_movies:
+        poster,title,sim_score=m[0],m[1],m[2]
+        if int(sim_score)==1 or title.lower() in added:
+            continue
+        id=getIndexFromTitle(title.lower())
+        query2="INSERT INTO recommendations(user_id,movie_id,score) VALUES(%s,%s,%s)"
+        values=(get_user_id(user),id,sim_score)
+        cursor.execute(query2,values)
+        topRecommendationCache[session['user']].add(title.lower())
+        added.add(title.lower())
+    mysql.get_db().commit()
+    print(top_movies)
+    return top_movies
+
+def get_user_id(user):
+    cursor=mysql.get_db().cursor()
+    query="SELECT user_id FROM users WHERE username=%s"
+    values=(user,)
+    cursor.execute(query,values)
+    res=cursor.fetchall()[0]
+    return res
+    
 
 @app.route("/poster/<movie>")
 def home(movie):
@@ -195,33 +224,33 @@ def user_home(user):
     cursor.execute(query,values)
     result=cursor.fetchall()
     if result:
-        count=0
-        if r.exists(user):
-            topMovies=r.lrange(user,0,-1)
+        if session['user'] in topRecommendationCache:  ## user has entries in recommendations
+            topMovies=topRecommendationCache[session['user']]
             movie_list=[]
             for movie in topMovies:
                 movie_list.append([get_movie_poster(movie.lower()),get_proper_title(movie)])
             return render_template('user_home.html',top_movies=movie_list)
-        seen=set()
-        for username,movie_id, movie_title in result:
-            movie_title=movie_title.lower()
-            min_heap=get_top_recommendations(movie_title,movie_id,True)
-            
-            for val in min_heap:
-                if val[1] not in seen and val[0]:
-                    r.rpush(user,val[1])
-                    seen.add(val[1])
-                    val[1]=get_proper_title(val[1])
-                    top_movies.append(val)
-        
-        return render_template('user_home.html',top_movies=top_movies[1:21])
+        else:
+            seen=set()
+            # user has no entries in recommendations
+            movie_user_likes=set([i[1] for i in result])
+            for username,movie_id, movie_title in result:
+                movie_title=movie_title.lower()
+                min_heap=get_top_recommendations(movie_title,movie_id,True) #[poster,title,sim_score]
+                
+                for val in min_heap:
+                    if val[1] not in seen and val[0] and val[1] not in movie_user_likes:
+                        seen.add(val[1])
+                        top_movies.append(val)
+                top_movies=filter_movies(top_movies)
+            return render_template('user_home.html',top_movies=top_movies[1:21])
     else:
         return redirect(url_for('like_movies',user=user))
 
 @app.route('/like_movies/<user>')
 def like_movies(user):
     movies=generate_list()
-    movies=[[get_movie_poster(movie),movie] for movie in movies]
+    movies=[[get_movie_poster(movie),get_proper_title(movie)] for movie in movies]
     return render_template('like_movies.html',sources=movies)
 
 @app.route('/initial-liked-movies/<user>',methods=["POST"])
@@ -235,7 +264,6 @@ def extract_movies(user):
     
     query1="INSERT INTO movie_likes(user_id,movie_id,movie_title) VALUES(%s,%s,%s)"
     for movie in checked_values:
-        movie=movie.lower()
         movie_id=getIndexFromTitle(movie)
         values=(id,movie_id,movie)
         cursor.execute(query1,values)
@@ -297,6 +325,8 @@ def like_movie():
         values=(user_id,movie_id,movie_title)
         cursor.execute(query,values)
         mysql.get_db().commit()
+        return "success"
+    return "already liked this movie"
 
 @app.route("/signup/")
 def signupPage():
@@ -314,35 +344,79 @@ def verify_signup():
     cursor = mysql.get_db().cursor()
 
     query = "SELECT * FROM users WHERE username=%s"
-    try:
-        cursor.execute(query, (username,))
-        result = cursor.fetchone()  # Fetch one row
-        if result:
-            session['error']="Username already exists"
-            return redirect(url_for('signupPage'))
-        else:
-            query = "INSERT INTO users (username, password) VALUES (%s, %s)"
-            values = (username, password)
-            try:
-                cursor.execute(query, values)
-                mysql.get_db().commit()  # Commit the transaction
-            except Exception as e:
-                print(f"Error: {e}")
-                mysql.get_db().rollback()  # Rollback the transaction in case of an error
-            cursor.close()
-            session['user']=username
-            if 'error' in session.keys():
-                session.pop('error')
-            return redirect(url_for('user_home',user=username))
-    except Exception as e:
-        return jsonify("error occurred")
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()  # Fetch one row
+    if result:
+        session['error']="Username already exists"
+        return redirect(url_for('signupPage'))
+    else:
+        query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+        values = (username, password)
+        try:
+            cursor.execute(query, values)
+            mysql.get_db().commit()  # Commit the transaction
+        except Exception as e:
+            print(f"Error: {e}")
+            mysql.get_db().rollback()  # Rollback the transaction in case of an error
+        cursor.close()
+        session['user']=username
+        if 'error' in session.keys():
+            session.pop('error')
+        return redirect(url_for('user_home',user=username))
 
 @app.route("/update-user-profile",methods=["POST"])
 def update():
     movie=request.form.get('movie')
-    return jsonify("recieved")
+    user=session['user']
+    id=get_user_id(user)
+    query="SELECT * FROM recommendations WHERE user_id=%s"
+    values=(id,)
+    cursor=mysql.get_db().cursor()
+    cursor.execute(query,values)
+    min_heap=[]
+    r=cursor.fetchall()
+    seen=set()
+    for row in r:
+        id=row[0]
+        user_id=row[1]
+        movie_id=row[2]
+        score=row[3]
+        if movie_id not in seen:
+            heapq.heappush(min_heap,[score,movie_id])
+            seen.add(movie_id)
+    movie_recs=get_top_recommendations(movie,getIndexFromTitle(movie))  #[poster,title,score]
+    for movie in movie_recs:
+        title=movie[1]
+        score=movie[2]
+        if len(min_heap)<30 and getIndexFromTitle(title) not in seen:
+            if int(score)==1:
+                continue
+            heapq.heappush(min_heap,[score,getIndexFromTitle(title)])
+            seen.add(getIndexFromTitle(title))
+        elif len(min_heap)>=30 and score>min_heap[0][0] and getIndexFromTitle(title) not in seen:
+            heapq.heappop(min_heap)
+            heapq.heappush(min_heap,[score,getIndexFromTitle(title)])
+    query="DELETE FROM recommendations WHERE user_id=%s"
+    values=(user_id,)
+    cursor.execute(query,values)
+    mysql.get_db().commit()
+    user_id=get_user_id(session['user'])
+    query="INSERT INTO recommendations(user_id,movie_id,score) VALUES(%s,%s,%s)"
+    #update cache
+    topRecommendationCache[session['user']]=set()
+    count=0
+    for rec in min_heap:
+        values=(user_id,rec[1],rec[0])
+        cursor.execute(query,values)
+        if count<20:
+            topRecommendationCache[session['user']].add(getTitleFromIndex(rec[1]).lower())
+        count+=1
+    mysql.get_db().commit()
+    print(topRecommendationCache)
+    return jsonify(movie_recs[0])
+    
 
-
+topRecommendationCache={}
 indexCache=Cache(1000)
 titleCache=Cache(1000)
     
