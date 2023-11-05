f6b1c1062533d723773accdef908a60eb499d32e
Justin <justinfisher340@gmail.com>
2023-11-05 13:24:24 -0500

    ajax requests for retrieval of genres in explore page

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
     var likeForms = document.getElementsByClassName('movie-form');
 
+    var genreForms=document.getElementsByClassName('bubble')
+
+    for(let i=0;i<genreForms.length;i++){
+        var genre_button=genreForms[i].querySelector('#g'+(i+1))
+
+        if(genre_button){
+            genre_button.addEventListener("click",(e)=>{
+                e.preventDefault();
+                
+                var formData=new FormData(genreForms[i]);
+                fetch('/find-genres',{
+                    method:"POST",
+                    body:formData,
+                }).then(res=>res.json()).then(data=>{
+                    var idx=0
+                    var parent = document.getElementById("poster-container");
+                    var children = parent.getElementsByClassName("poster");
+                    for(let movie in data){
+                        if (data.hasOwnProperty(movie)) {
+                            if(children&&children[idx]){
+                                children[idx].src=data[movie]
+                                idx++
+                            }
+                            else{
+                                var child = document.createElement("img");
+                                child.src = data[movie]; 
+                                child.className="poster"
+                                parent.appendChild(child);
+                            }
+                        }
+                    }
+                })
+            })
+        }
+    }
+
     for (let i = 0; i < likeForms.length; i++) {
         var id = "like-button" + (i + 1); 
 
